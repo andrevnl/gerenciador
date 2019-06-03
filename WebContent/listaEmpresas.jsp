f14e0897bbec4db72852d7a288960f8fc78a1a79
andrevnl <andrevnl@hotmail.com>
2019-06-02 22:09:48 -0300

    Começando a colocar no Padrão MVC

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -1,10 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-<%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa"%>
+<%@ page import="java.util.List,br.com.alura.gerenciador.modelo.Empresa"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
-<c:url value="/mostraEmpresa" var="linkServletEditarEmpresa"/>
-<c:url value="/removeEmpresa" var="linkServletRemoverEmpresa"/>
+<c:url value="entrada?acao=MostraEmpresa" var="linkServletEditarEmpresa"/>
+<c:url value="entrada?acao=RemoveEmpresa" var="linkServletRemoverEmpresa"/>
 
 <!DOCTYPE html>
 <html>
@@ -20,8 +20,8 @@
 		<c:forEach items="${empresas}" var="empresa" >
 			<li>
 				${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/> 
-				<a href="${linkServletEditarEmpresa}?id=${empresa.id}">editar</a>
-				<a href="${linkServletRemoverEmpresa}?id=${empresa.id}">remover</a>
+				<a href="${linkServletEditarEmpresa}&id=${empresa.id}">editar</a>
+				<a href="${linkServletRemoverEmpresa}&id=${empresa.id}">remover</a>
 			</li>
 		</c:forEach>
 	</ul>
--- src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,21 +6,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AlteraEmpresaServlet
- */
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Alterando empresa");
-		
+public class AlteraEmpresa {
+
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");
 		String paramId = request.getParameter("id");
@@ -35,8 +29,6 @@ public class AlteraEmpresaServlet extends HttpServlet {
 			throw new ServletException(e);
 		}
 		
-		System.out.println(id);
-		
 		Banco banco = new Banco();
 		
 		Empresa empresa = banco.buscarEmpresaPelaId(id);
@@ -44,7 +36,7 @@ public class AlteraEmpresaServlet extends HttpServlet {
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
 		
-		response.sendRedirect("listaEmpresas");
+		response.sendRedirect("entrada?acao=ListaEmpresas");
 	}
 
 }
