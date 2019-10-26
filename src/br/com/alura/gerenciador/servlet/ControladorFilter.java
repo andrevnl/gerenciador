ee0cd79b6f4dc8676ed78376feeca00db0468841
andrevnl <andrevnl@hotmail.com>
2019-10-26 11:01:23 -0300

    Adicionando init e destroy dentro dos filters

--- src/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ src/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -3,19 +3,24 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
 //@WebFilter("/entrada")
 public class ControladorFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
